3d876f2c1d2a6426598c5ae6fcc00592a3552db3
Ilies Hadj <ihadj@f6r10s5.clusters.42paris.fr>
2025-06-06 17:03:17 +0200

    PUSH05

--- srcs/sorting/cases.c
+++ srcs/sorting/cases.c
@@ -6,13 +6,13 @@
 /*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 18:29:05 by ihadj             #+#    #+#             */
-/*   Updated: 2025/06/06 17:01:45 by ihadj            ###   ########.fr       */
+/*   Updated: 2025/06/06 17:03:11 by ihadj            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	best_case(t_piles *p, t_node *el_a, int cost_a, int cost_b)
+int	best_case(t_piles *p, t_node *el_a)
 {
 	int		c1;
 	int		c2;
